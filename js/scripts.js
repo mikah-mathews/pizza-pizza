7f4ae2d1069a3fa18b0634a7794d945e18a77963
Mikah Mathews <mikah@outofband.org>
2022-01-28 10:40:01 -0800

    Add price prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,18 +3,20 @@ function Pizza(toppings, size) {
   this.size = size;
 }
 
-Pizza.prototype.price = function() {
-  if (this.size === "small") {
+Pizza.prototype.price = function(order) {
+  if (order === "small") {
     console.log("10");
-  } else if (this.size === "medium") {
+    return 10;5 
+  } else if (order === "medium") {
     console.log("12")
     return 12;
-  } else if (this.size === "large") {
+  } else if (order === "large") {
     console.log("15")
     return 15;
   }
 }
 
 let newPizza = new Pizza(["pepperoni", "pineapple"], "small");
+let size = newPizza
 console.log(newPizza);
-console.log(newPizza.price);
+console.log(newPizza.price("small"));
