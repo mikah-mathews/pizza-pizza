2e4cffbeaa2f89b394f9f067ee634e1baa9c948f
Mikah Mathews <mikah@outofband.org>
2022-01-28 15:25:47 -0800

    Add js to find selected pizza size

--- js/scripts.js
+++ js/scripts.js
@@ -5,29 +5,32 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.price = function(order) {
   if (order === "small") {
-    console.log("10");
+    console.log("That will be $10");
     return 10;
   } else if (order === "medium") {
-    console.log("12")
+    console.log("That will be $12")
     return 12;
   } else if (order === "large") {
-    console.log("15")
+    console.log("That will be $15")
     return 15;
   }
 }
 
-let newPizza = new Pizza(["pepperoni", "pineapple"], "small");
-console.log(newPizza);
-console.log(newPizza.price("small"));
+// let newPizza = new Pizza(["pepperoni", "pineapple"], "small");
+// console.log(newPizza);
+// console.log(newPizza.price("small"));
 
 $(document).ready(function() {
   $("#pizza-builder").submit(function(event) {
     event.preventDefault();
+    let userSize = $("input:radio[name=size]:checked").val();
     let userToppings = [];
     $("input:checkbox[name='topping']:checked").each(function(){
       userToppings.push($(this).val());
-    });
-    console.log(userToppings);
+    });  // Could this be refactored as it's own function to be called?
+    let userPizza = new Pizza(userToppings, userSize);
+    console.log(userPizza);
+    console.log(userPizza.price(userSize));
   });  
 });
 
