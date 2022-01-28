d86a2a1cab444b8e26eeaef2920b2962230a5e64
Mikah Mathews <mikah@outofband.org>
2022-01-28 15:52:48 -0800

    Add user topping selection results, pizza price results, and clear form input/previous orders

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Backend logic
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
@@ -5,32 +6,29 @@ function Pizza(toppings, size) {
 
 Pizza.prototype.price = function(order) {
   if (order === "small") {
-    console.log("That will be $10");
     return 10;
   } else if (order === "medium") {
-    console.log("That will be $12")
     return 12;
   } else if (order === "large") {
-    console.log("That will be $15")
     return 15;
   }
 }
 
-// let newPizza = new Pizza(["pepperoni", "pineapple"], "small");
-// console.log(newPizza);
-// console.log(newPizza.price("small"));
-
+// UI logic
 $(document).ready(function() {
   $("#pizza-builder").submit(function(event) {
     event.preventDefault();
+    $("#toppingsOutput").empty();
+    $("#finalPrice").empty();
     let userSize = $("input:radio[name=size]:checked").val();
     let userToppings = [];
     $("input:checkbox[name='topping']:checked").each(function(){
-      userToppings.push($(this).val());
+      userToppings.push(" " + $(this).val() + " ");
     });  // Could this be refactored as it's own function to be called?
     let userPizza = new Pizza(userToppings, userSize);
-    console.log(userPizza);
-    console.log(userPizza.price(userSize));
+    $("#toppingsOutput").append("The toppings selected are: " + userToppings);
+    $("#finalPrice").append("$" + userPizza.price(userSize));
+    $('input[type="checkbox"]').prop('checked', false);
   });  
 });
 
