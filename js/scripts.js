c9906df0b0b97ca0d3fa7ced943f0e5bbd03f804
Mikah Mathews <mikah@outofband.org>
2022-01-28 14:00:22 -0800

    Add function to get the selected topping values and push to an array

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,14 @@ Pizza.prototype.price = function(order) {
 let newPizza = new Pizza(["pepperoni", "pineapple"], "small");
 console.log(newPizza);
 console.log(newPizza.price("small"));
+
+$(document).ready(function() {
+  $("#pizza-builder").submit(function(event) {
+    event.preventDefault();
+    let userToppings = [];
+    $("input:checkbox[name='topping']:checked").each(function(){
+      userToppings.push($(this).val());
+    });
+    console.log(userToppings);
+  })  
+})
